21393dac88d4af57d02da76f35dcf8feb198ba47
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-10-06 20:19:20 -0700

    52' add some elements to register page
    - inputs email and password and button register
    - add handler for register button click

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="register">
     <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model=email
+      placeholder="email"/>
+    <br>
+
+    <input
+      type="password"
+      name="password"
+      v-model=password
+      placeholder="password"/>
+    <br>
+
+    <button @click="register">Register</button>
   </div>
 </template>
 
@@ -9,7 +24,27 @@ export default {
   name: 'register',
   data () {
     return {
+      email: '',
+      password: ''
     }
+  },
+
+  watch: {
+    email (value) {
+      console.log(`email has changed ${value}`)
+    }
+  },
+
+  methods: {
+    register () {
+      console.log('register button was clicked!')
+    }
+  },
+
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 2000)
   }
 }
 </script>
