3e109b8b7af079c8ca382bdb4a5ce977310e0aef
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-10-13 23:13:32 -0700

    42' client: add create song component

---
+++ client/src/components/CreateSong.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <panel title="Song Metadada">
+      <v-text-field
+        v-model="title"
+        label="Title"
+        id="input-title"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="artist"
+        label="Artist"
+        id="input-artist"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="genre"
+        label="Genre"
+        id="input-genre"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="album"
+        label="Album"
+        id="input-album"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="albumImageUrl"
+        label="Album Image URL"
+        id="input-album-image-url"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="youtubeId"
+        label="YouTube ID"
+        id="input-youtube-id"
+      ></v-text-field>
+    </panel>
+  </div>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+
+export default {
+  data () {
+    return {
+      title: null,
+      artist: null,
+      genre: null,
+      album: null,
+      albumImageUrl: null,
+      youtubeId: null,
+      lyrics: null,
+      tab: null
+    }
+  },
+
+  components: {
+    Panel
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
