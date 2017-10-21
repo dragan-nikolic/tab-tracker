2ca2929de7341280f0bc17350982eb33366ff1bf
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-10-20 23:32:46 -0700

    12' client: add text input filed for song search
    and sync it with URL query string

--- client/src/components/Songs/SongsSearchPanel.vue
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -1,12 +1,42 @@
 <template>
   <panel title="Search">
-
+    <v-text-field
+      v-model="search"
+      label="Search by song title, artist, album, or genre"
+      id="input-search"
+    ></v-text-field>
   </panel>
 </template>
 
 <script>
 export default {
-  components: {
+  data () {
+    return {
+      search: ''
+    }
+  },
+
+  watch: {
+    search (value) {
+      const route = {
+        name: 'songs'
+      }
+
+      if (this.search !== '') {
+        route.query = {
+          search: this.search
+        }
+      }
+
+      this.$router.push(route)
+    },
+
+    '$route.query.search': {
+      immediate: true,
+      handler (value) {
+        this.search = value
+      }
+    }
   }
 }
 </script>
