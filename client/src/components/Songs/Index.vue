e954b63c2e4656ab37f1ecda91579dcaad778b70
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-10-20 22:39:43 -0700

    5' client: refactor Songs component
    add SongsSearchPanel (empty for now)

---
+++ client/src/components/Songs/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <songs-search-panel />
+      <songs-panel class="mt-2"/>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongsPanel from './SongsPanel'
+import SongsSearchPanel from './SongsSearchPanel'
+
+export default {
+  components: {
+    SongsPanel,
+    SongsSearchPanel
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -0,0 +1,18 @@
+<template>
+  <panel title="Search">
+
+  </panel>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+</style>
