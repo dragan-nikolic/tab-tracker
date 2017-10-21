f6bdb6b6c725a20e1474ba9f13f3eb57ebe85ce9
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-10-20 23:31:32 -0700

    fix Panel - title and create button

--- client/src/components/globals/Panel.vue
+++ client/src/components/globals/Panel.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="white elevation-2">
     <v-toolbar dark flat dense class="cyan">
-      </v-toolbar-title>{{title}}</v-toolbar-title>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
       <slot name="action" />
     </v-toolbar>
 
     <div class="pl-4 pr-4 pt-2 pb-2">
       <slot>
-        No slot content defined.
+        No content defined.
       </slot>
     </div>
     
